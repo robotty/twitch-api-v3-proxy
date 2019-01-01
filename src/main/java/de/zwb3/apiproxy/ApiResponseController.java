f3106011d596fdc282ca311bbc56dfcd7e79a965
RAnders00 <rubenanders@outlook.de>
2019-01-01 02:34:19 +0100

    Proxy request message body to twitch on POST requests, fixes POST endpoints timing out

--- src/main/java/de/zwb3/apiproxy/ApiResponseController.java
+++ src/main/java/de/zwb3/apiproxy/ApiResponseController.java
@@ -9,6 +9,7 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.methods.RequestBuilder;
 import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.InputStreamEntity;
 import org.apache.http.impl.client.HttpClients;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -68,38 +69,15 @@ public class ApiResponseController {
         // mappedPath is for example "/kraken/streams/22484632"
         String mappedPath = mapper.mapApiPath(request.getMethod(), request.getRequestURI());
 
-        URI requestURI = new URIBuilder()
+        URI proxyUri = new URIBuilder()
                 .setScheme("https")
                 .setHost("api.twitch.tv")
                 .setPath(mappedPath)
                 .setQuery(request.getQueryString())
                 .build();
 
-        HttpUriRequest proxyRequest = RequestBuilder.create(request.getMethod())
-                .setUri(requestURI)
-                .addHeader("Accept", "application/vnd.twitchtv.v5+json")
-                .build();
-
-        // copy input headers to proxy request, except for "Accept" header
-        Enumeration<String> headerNames = request.getHeaderNames();
-        while (headerNames.hasMoreElements()) {
-            String headerName = headerNames.nextElement();
-
-            // don't proxy the Accept header, since we set our own Accept header for API v5.
-            if ("Accept".equalsIgnoreCase(headerName) || "Host".equalsIgnoreCase(headerName)) {
-                continue;
-            }
-
-            Enumeration<String> headerValues = request.getHeaders(headerName);
-            while (headerValues.hasMoreElements()) {
-                String headerValue = headerValues.nextElement();
-
-                proxyRequest.addHeader(headerName, headerValue);
-            }
-        }
-
-        HttpClient client = getHttpClient();
-        HttpResponse proxyResponse = client.execute(proxyRequest);
+        // request is the received request, proxyUri is the URI to make the proxy request to.
+        HttpResponse proxyResponse = makeProxyRequest(request, proxyUri);
 
         // copy status
         response.setStatus(proxyResponse.getStatusLine().getStatusCode());
@@ -120,6 +98,58 @@ public class ApiResponseController {
             IOUtils.copy(bufferedInputStream, bufferedOutputStream);
         }
 
+    }
+
+    /**
+     * Make a proxy request similar to the given originalRequest, but to the given proxyUri.
+     * @param originalRequest The request that was sent to this application.
+     * @param proxyUri The URI to proxy to.
+     * @return The response of the proxy request.
+     * @throws IOException If an I/O exception occurs.
+     */
+    private HttpResponse makeProxyRequest(HttpServletRequest originalRequest, URI proxyUri) throws IOException {
+        HttpResponse proxyResponse;
+        try (InputStream inputStream = originalRequest.getInputStream();
+             BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream)) {
+
+            // If this is a POST request, copy the request body.
+            InputStreamEntity entity;
+            if ("POST".equalsIgnoreCase(originalRequest.getMethod())) {
+                entity = new InputStreamEntity(bufferedInputStream);
+            } else {
+                entity = null;
+            }
+
+            HttpUriRequest proxyRequest = RequestBuilder.create(originalRequest.getMethod())
+                    .setUri(proxyUri)
+                    .addHeader("Accept", "application/vnd.twitchtv.v5+json")
+                    .setEntity(entity)
+                    .build();
+
+            // copy input headers to proxy request, except for "Accept" header
+            Enumeration<String> headerNames = originalRequest.getHeaderNames();
+            while (headerNames.hasMoreElements()) {
+                String headerName = headerNames.nextElement();
+
+                // don't proxy the Accept header, since we set our own Accept header for API v5.
+                if ("Accept".equalsIgnoreCase(headerName) ||
+                        "Host".equalsIgnoreCase(headerName) ||
+                        "Content-Length".equalsIgnoreCase(headerName)) {
+                    continue;
+                }
+
+                Enumeration<String> headerValues = originalRequest.getHeaders(headerName);
+                while (headerValues.hasMoreElements()) {
+                    String headerValue = headerValues.nextElement();
+
+                    proxyRequest.addHeader(headerName, headerValue);
+                }
+            }
+
+            HttpClient client = getHttpClient();
+            proxyResponse = client.execute(proxyRequest);
+        }
+        return proxyResponse;
     }
 
     /**
